ed6f2966fecf421a813e92857d147cb61f5e4e00
James Brannigan <jamesbrannigan89@gmail.com>
2020-02-04 22:21:19 +0000

    added ngfor to display cards. Removed hard coded html cards

--- src/app/countries-visited/countries-visited.component.ts
+++ src/app/countries-visited/countries-visited.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-countries-visited',
@@ -6,6 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./countries-visited.component.css']
 })
 export class CountriesVisitedComponent implements OnInit {
+
+  
+  
+
+    countries =[
+      {
+      //  image:'src="/assets/img/austria.jpg"',
+        country:'Austria',
+        description:'We visited Austria three different times throught our trip and went to three different cities. Click below to see more photos of Austria.',
+        imageUrl:"/assets/img/austria.jpg"
+      },
+
+      {
+        id:2,
+        country:'Belgium',
+        description:'test',
+        imageUrl:"/assets/img/belgium.jpg"
+      }
+      
+    ]
+
+  
 
   constructor() { }
 
